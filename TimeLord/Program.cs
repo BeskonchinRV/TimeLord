5a883a80fddd5f113a3c37a50f8bf748f66579e1
BeskonchinRV <BeskonchinRV@411D-4>
2023-04-08 10:22:04 +0700

    Добавьте файлы проекта.

---
+++ TimeLord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeLord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
